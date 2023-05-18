5f81fe68b0cf1f68d105cc904b1828e75cb9e991
Sean Marlow <sean.marlow@suse.com>
2023-05-18 09:25:46 -0400

    Bump version: 0.0.2 → 0.1.0

--- csp_billing_adapter/__init__.py
+++ csp_billing_adapter/__init__.py
@@ -30,7 +30,7 @@ import pluggy
 
 __author__ = """SUSE"""
 __email__ = 'public-cloud-dev@susecloud.net'
-__version__ = '0.0.2'
+__version__ = '0.1.0'
 
 hookimpl = pluggy.HookimplMarker('csp_billing_adapter')
 """Marker to be imported and used in plugins (and for own implementations)"""
