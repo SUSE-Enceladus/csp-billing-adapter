fa19cbd57e4b740a630452028bd3b99d8f5deaba
Robert Schweikert <rjschwei@suse.com>
2023-08-18 15:31:58 -0400

    Bump version: 0.4.0 → 0.5.0

--- tests/unit/test_adapter.py
+++ tests/unit/test_adapter.py
@@ -50,7 +50,7 @@ from csp_billing_adapter.utils import (
 
 def test_adapter_version():
     """Verify the adapter version."""
-    assert csp_billing_adapter.__version__ == "0.4.0"
+    assert csp_billing_adapter.__version__ == "0.5.0"
 
 
 def test_get_plugin_manager(cba_config):
