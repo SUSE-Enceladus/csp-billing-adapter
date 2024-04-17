78626be7efe1f4a2e3515cbd184e3dae7f1b2961
Sean Marlow <sean.marlow@suse.com>
2024-04-17 11:15:35 -0400

    Bump version: 0.9.0 → 0.10.0

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ with open('requirements-dev.txt') as req_file:
 
 setup(
     name='csp-billing-adapter',
-    version='0.9.0',
+    version='0.10.0',
     description='TBD',
     long_description=readme,
     long_description_content_type='text/markdown',
